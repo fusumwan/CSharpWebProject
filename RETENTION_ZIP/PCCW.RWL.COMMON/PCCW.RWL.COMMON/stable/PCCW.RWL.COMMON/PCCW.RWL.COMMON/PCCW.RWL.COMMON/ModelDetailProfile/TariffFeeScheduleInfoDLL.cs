ca7d79b814a6d4dbdf314efddbd42c3b930f12f0
MSI\timot <timothyfuapple@gmail.com>
2023-11-20 17:10:20 +1030

    Upload the PCCW.RWL.COMMON Files (Part 6)

---
+++ RETENTION_ZIP/PCCW.RWL.COMMON/PCCW.RWL.COMMON/stable/PCCW.RWL.COMMON/PCCW.RWL.COMMON/PCCW.RWL.COMMON/ModelDetailProfile/TariffFeeScheduleInfoDLL.cs
@@ -0,0 +1,395 @@
+using System;
+using System.Data;
+using System.Text;
+using System.Globalization;
+using System.Configuration;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Xml;
+using NEURON.ENTITY.FRAMEWORK;
+
+using NEURON.ENTITY.FRAMEWORK.SESSIONFACTORY;
+using NEURON.ENTITY.FRAMEWORK.CONNECT;
+///-- =============================================
+///-- Author:		<Author: Sum Wan,FU>
+///-- Create date: <Create Date 2009-02-03>
+///-- Description:	<Description,Table :[TariffFeeSchedule], Information layer>
+///-- =============================================
+namespace PCCW.RWL.CORE{
+    
+    /// <summary>
+    /// Summary description for table [TariffFeeSchedule] Information layer
+    /// </summary>
+    public abstract class TariffFeeScheduleInfoDLL{
+        
+        
+        #region Parameter
+        private TableObj n_oTableSet = new TableObj();
+        public bool n_bViewAll=false;
+        public string n_sPrefix = Configurator.MSSQLTablePrefix;
+        #endregion
+        
+        #region Constructor
+        
+        public TariffFeeScheduleInfoDLL(){
+            Init();
+        }
+        ~TariffFeeScheduleInfoDLL(){
+            this.Release();
+        }
+        #endregion
+
+        #region Create Instance Object
+        public static TariffFeeScheduleInfo CreateInfoInstanceObject()
+        {
+            return new TariffFeeScheduleInfo();
+        }
+        #endregion
+        
+        #region Init
+        public void Init()
+        {
+            
+            string _sStrFldName;
+            n_oTableSet.TableName = TariffFeeSchedule.Para.TableName();
+            n_oTableSet.DefaultFilter = String.Empty;
+            n_oTableSet.GroupBy = String.Empty;
+            n_oTableSet.Having = String.Empty;
+            n_oTableSet.DefaultOrderBy = String.Empty;
+            
+            // [id] Field
+            _sStrFldName = TariffFeeSchedule.Para.id;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = true;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = true;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = true;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = true;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[id]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "System.Nullable<int>";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "id";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[id]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "id";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "id";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "id";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = false;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = false;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = false;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = false;
+            n_oTableSet.Fields(_sStrFldName).Size = 0;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "int";
+            
+            
+            // [cdate] Field
+            _sStrFldName = TariffFeeSchedule.Para.cdate;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = false;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = false;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[cdate]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "System.Nullable<DateTime>";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "cdate";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[cdate]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "cdate";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "cdate";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "cdate";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = true;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = true;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = true;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = true;
+            n_oTableSet.Fields(_sStrFldName).Size = 0;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "datetime";
+            
+            
+            // [cid] Field
+            _sStrFldName = TariffFeeSchedule.Para.cid;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = false;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = false;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[cid]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "string";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "cid";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[cid]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "cid";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "cid";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "cid";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = true;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = true;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = true;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = true;
+            n_oTableSet.Fields(_sStrFldName).Size = 10;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "char";
+            
+            
+            // [active] Field
+            _sStrFldName = TariffFeeSchedule.Para.active;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = false;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = false;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[active]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "System.Nullable<bool>";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "active";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[active]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "active";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "active";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "active";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = true;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = true;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = true;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = true;
+            n_oTableSet.Fields(_sStrFldName).Size = 0;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "bit";
+            
+            
+            // [program] Field
+            _sStrFldName = TariffFeeSchedule.Para.program;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = false;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = false;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[program]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "string";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "program";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[program]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "program";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "program";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "program";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = true;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = true;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = true;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = true;
+            n_oTableSet.Fields(_sStrFldName).Size = 50;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "nvarchar";
+            
+            
+            // [ddate] Field
+            _sStrFldName = TariffFeeSchedule.Para.ddate;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = false;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = false;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[ddate]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "System.Nullable<DateTime>";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "ddate";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[ddate]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "ddate";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "ddate";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "ddate";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = true;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = true;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = true;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = true;
+            n_oTableSet.Fields(_sStrFldName).Size = 0;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "datetime";
+            
+            
+            // [org_fee] Field
+            _sStrFldName = TariffFeeSchedule.Para.org_fee;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = false;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = false;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[org_fee]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "System.Nullable<int>";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "org_fee";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[org_fee]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "org_fee";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "org_fee";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "org_fee";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = true;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = true;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = true;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = true;
+            n_oTableSet.Fields(_sStrFldName).Size = 0;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "int";
+            
+            
+            // [did] Field
+            _sStrFldName = TariffFeeSchedule.Para.did;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = false;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = false;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[did]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "string";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "did";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[did]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "did";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "did";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "did";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = true;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = true;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = true;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = true;
+            n_oTableSet.Fields(_sStrFldName).Size = 10;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "char";
+            
+            
+            // [fee] Field
+            _sStrFldName = TariffFeeSchedule.Para.fee;
+            n_oTableSet.Add(_sStrFldName);
+            n_oTableSet.Fields(_sStrFldName).IsAutoIncrement = false;
+            n_oTableSet.Fields(_sStrFldName).IsIdentityKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsPrimaryKey = false;
+            n_oTableSet.Fields(_sStrFldName).IsUniqueKey = false;
+            n_oTableSet.Fields(_sStrFldName).FieldName = "[fee]";
+            n_oTableSet.Fields(_sStrFldName).FieldType = "string";
+            n_oTableSet.Fields(_sStrFldName).FieldVar = "fee";
+            n_oTableSet.Fields(_sStrFldName).FieldUploadPath = "";
+            n_oTableSet.Fields(_sStrFldName).SortName = "[fee]";
+            n_oTableSet.Fields(_sStrFldName).SortParm = "fee";
+            n_oTableSet.Fields(_sStrFldName).AliasName = "fee";
+            n_oTableSet.Fields(_sStrFldName).ParameterName = "fee";
+            n_oTableSet.Fields(_sStrFldName).QuoteStart = "";
+            n_oTableSet.Fields(_sStrFldName).QuoteEnd = "";
+            n_oTableSet.Fields(_sStrFldName).IsList = true;
+            n_oTableSet.Fields(_sStrFldName).IsView = true;
+            n_oTableSet.Fields(_sStrFldName).IsEdit = true;
+            n_oTableSet.Fields(_sStrFldName).IsDelete = true;
+            n_oTableSet.Fields(_sStrFldName).IsAdd = true;
+            n_oTableSet.Fields(_sStrFldName).IsSearch = true;
+            n_oTableSet.Fields(_sStrFldName).IsRegister = false;
+            n_oTableSet.Fields(_sStrFldName).IsNullable = true;
+            n_oTableSet.Fields(_sStrFldName).Size = 50;
+            n_oTableSet.Fields(_sStrFldName).SqlDataType = "nvarchar";
+            
+        }
+        #endregion
+        
+        
+        #region Get & Set
+        
+        #region GetViewAll
+        public bool GetViewAll(){ return n_bViewAll; }
+        #endregion GetViewAll
+        
+        
+        #region SetViewAll
+        public void SetViewAll(bool value){  n_bViewAll=value; }
+        #endregion SetViewAll
+        
+        
+        #region GetTableSet
+        public TableObj GetTableSet(){ return n_oTableSet; }
+        #endregion GetTableSet
+        
+        
+        #region SetTableSet
+        public void SetTableSet(TableObj value){ n_oTableSet=value; }
+        #endregion SetTableSet
+        
+        #region GetTableInfo
+        public Field GetIdTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.id);
+        }
+        public Field GetCdateTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.cdate);
+        }
+        public Field GetCidTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.cid);
+        }
+        public Field GetActiveTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.active);
+        }
+        public Field GetProgramTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.program);
+        }
+        public Field GetDdateTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.ddate);
+        }
+        public Field GetOrg_feeTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.org_fee);
+        }
+        public Field GetDidTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.did);
+        }
+        public Field GetFeeTable(){
+            return n_oTableSet.Fields(TariffFeeSchedule.Para.fee);
+        }
+        
+        public Field Fields(string x_sColumn){
+            return n_oTableSet.Fields(x_sColumn);
+        }
+        #endregion GetTableInfo
+        #endregion
+        #region
+        public void Release(){}
+        #endregion
+    }
+}
+
+
---
+++ RETENTION_ZIP/PCCW.RWL.COMMON/PCCW.RWL.COMMON/stable/PCCW.RWL.COMMON/PCCW.RWL.COMMON/PCCW.RWL.COMMON/TierViewItem/TierViewItemDAO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Globalization;
+using System.Configuration;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Data.Sql;
+using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Xml;
+using NEURON.ENTITY.FRAMEWORK;
+using NEURON.ENTITY.FRAMEWORK.SESSIONFACTORY;
+using NEURON.ENTITY.FRAMEWORK.CONNECT;
+namespace PCCW.RWL.CORE
+{
+
+    /// <summary>
+    /// Summary description for TierViewItemDAO
+    /// </summary>
+    public class TierViewItemDAO
+    {
+        public TierViewItemDAO()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        public IList<TierViewItem> FindSuccess()
+        {
+            return TierViewControl.Instance().GetSuccessIn();
+        }
+
+        public IList<TierViewItem> FindError()
+        {
+
+            return TierViewControl.Instance().GetErrRecord();
+        }
+
+        public IList<TierViewItem> FindDuplicate()
+        {
+
+            return TierViewControl.Instance().GetDupRecord();
+        }
+    }
+}
